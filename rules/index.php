790843bcc434982e0028547c102c81ce93fda1e9
Block2Block <accounts@block2block.me>
2020-07-09 00:00:06 +0100

    Updated to start including other panels

---
+++ rules/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Home | AuroraMC Network Rules Committee | Admin Panel</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="css/navbar.css">
+</head>
+
+<body style="background-color: #23272A;color:white">
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-1 order-md-0">
+        <ul class="navbar-nav ml-auto text-center">
+            <li class="nav-item">
+            <a class="nav-link" href="chat">Chat Rules</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="game">Game Rules</a>
+            </li>
+        </ul>
+    </div>
+    <div class="mx-auto my-2 order-0 order-md-1 position-relative">
+        <a class="mx-auto" href="/rules/">
+            <img src="img/logo.png" height="100px" width="100px"
+                 style="margin-top:60px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-2 order-md-2">
+        <ul class="navbar-nav mr-auto text-center">
+            <li class="nav-item">
+                <a class="nav-link" href="misc">Misc Rules</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="archive">Archived Rules</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container-fluid" style="padding-top: 40px">
+    <div class="row">
+        <div class="col-sm-2"></div> <!-- Gap at left side of form -->
+        <div class="col-sm-8 col-xs-12">
+            <br>
+            <h1><Strong>AuroraMC Network Rules Committee Admin Panel</Strong></h1>
+            <br>
+            <legend style="font-family: 'Helvetica';">Welcome!</legend>
+            <hr>
+            <p style="font-size: 17px; font-family: 'Helvetica'">Welcome to the AuroraMC Network's Rules Committee Admin Panel! Here, you can see all active rules that appear in the Punishment GUI. You can modify, rename, archive and create rules using this Web GUI.</p>
+        </div>
+        <div class="col-sm-2"></div> <!-- Gap at right side of form -->
+    </div>
+</div>
+</body>
+</html>
--- rules/misc.php
+++ rules/misc.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Chat Rules | AuroraMC Network Rules Committee | Admin Panel</title>
+    <title>Misc Rules | AuroraMC Network Rules Committee | Admin Panel</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -18,29 +18,50 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <script src="js/rules.js"></script>
+    <link rel="stylesheet" href="css/navbar.css">
 </head>
 
 <body style="background-color: #23272A;color:white">
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <a class="navbar-brand" href="http://rules.auroramc.block2block.me/">AuroraMC Rules Committee - Admin Panel</a>
-
-    <ul class="nav navbar-nav">
-        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-        <li class="nav-item active"><a class="nav-link" href="#">Chat Rules</a></li>
-        <li class="nav-item"><a class="nav-link" href="game">Game Rules</a></li>
-        <li class="nav-item"><a class="nav-link" href="misc">Misc Rules</a></li>
-        <li class="nav-item"><a class="nav-link" href="archive">Archived Rules</a></li>
-    </ul>
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-1 order-md-0">
+        <ul class="navbar-nav ml-auto text-center">
+            <li class="nav-item">
+                <a class="nav-link" href="chat">Chat Rules</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="game">Game Rules</a>
+            </li>
+        </ul>
+    </div>
+    <div class="mx-auto my-2 order-0 order-md-1 position-relative">
+        <a class="mx-auto" href="/rules/">
+            <img src="img/logo.png" height="100px" width="100px"
+                 style="margin-top:60px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-2 order-md-2">
+        <ul class="navbar-nav mr-auto text-center">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Misc Rules</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="archive">Archived Rules</a>
+            </li>
+        </ul>
+    </div>
 </nav>
 
-<div class="container-fluid">
+<div class="container-fluid" style="padding-top: 40px">
     <div class="row">
         <div class="col-sm-2"></div> <!-- Gap at left side of form -->
         <div class="col-sm-8 col-xs-12">
             <br>
             <h1><Strong>AuroraMC Network Rules Committee Admin Panel</Strong></h1>
             <br>
-            <legend style="font-family: 'Helvetica';">Currently Active Chat Rules</legend>
+            <legend style="font-family: 'Helvetica';">Currently Active Misc Rules</legend>
             <br>
             <table class="table table-hover" style="color:white">
                 <thead>
@@ -55,11 +76,11 @@
                 </thead>
                 <tbody id="table-values">
                 <?php
-                include_once "database/db-connect.php";
+                include_once "../database/db-connect.php";
                 $weights = array("<p style='color:#00AA00'><Strong>Light</Strong></p>", "<p style='color:#55FF55'><Strong>Medium</Strong></p>","<p style='color:#FFFF55'><Strong>Heavy</Strong></p>","<p style='color:#FFAA00'><Strong>Severe</Strong></p>","<p style='color:#AA0000'><Strong>Extreme</Strong></p>");
                 $requires_warnings = array("<Strong>No</Strong>","<Strong>Yes</Strong>");
 
-                if ($sql = $mysqli->prepare("SELECT * FROM rules WHERE type = 1 AND active = 1 ORDER BY weight ASC, rule_id ASC")) {
+                if ($sql = $mysqli->prepare("SELECT * FROM rules WHERE type = 3 AND active = 1 ORDER BY weight ASC, rule_id ASC")) {
                     $sql->execute();    // Execute the prepared query.
 
                     $id = null;
@@ -85,7 +106,7 @@
                 </tbody>
             </table>
 
-            <button type="button" class="btn btn-success" onclick="newrule(1)" style="margin-left:50%;margin-right:50%;"><i class="fas fa-plus"></i> Create Rule</button>
+            <button type="button" class="btn btn-success" onclick="newrule(3)" style="margin-left:50%;margin-right:50%;"><i class="fas fa-plus"></i> Create Rule</button>
             <br>
             <br>
             <br>
