4001de860addd88692ef34cfad5cca6657d9e2a0
Block2Block <accounts@block2block.me>
2021-10-27 16:13:20 +0100

    Removed SHA512 encryption as it was redundant anyway as it is classed as insecure

--- js/main.js
+++ js/main.js
@@ -34,16 +34,6 @@ function formhash(form, username, password, code) {
     if (username.value === '' || password === '' || code.value === '') {
         return false;
     }
-    var p = document.createElement("input");
-
-    // Add the new element to our form.
-    form.appendChild(p);
-    p.name = "p";
-    p.type = "hidden";
-    p.value = hex_sha512(password.value);
-
-    // Make sure the plaintext password doesn't get sent.
-    password.value = "";
 
     // Finally submit the form.
     form.submit();
