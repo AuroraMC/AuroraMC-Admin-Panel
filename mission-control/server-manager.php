6fa746cd558ca167d497f7dba032c6b41bd16145
Block2Block <accounts@block2block.me>
2021-11-21 17:44:11 +0000

    Attempt 3

--- mission-control/server-manager.php
+++ mission-control/server-manager.php
@@ -83,7 +83,7 @@
                     <div class="col-sm-4">
                         <div class="card text-center mx-xl-5">
                             <div class="card-header">
-                                <h3 style="color:black;">CREATE A SERVER</h3>
+                                <p style="color:black;margin-bottom: 0">Create a server</p>
                             </div>
                             <!-- Card body -->
                             <div class="card-body">
