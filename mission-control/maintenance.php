a26868c136843f856a121fdf5634bfb4b52f5d23
Block2Block <accounts@block2block.me>
2021-11-21 22:03:16 +0000

    Made text black

--- mission-control/maintenance.php
+++ mission-control/maintenance.php
@@ -167,7 +167,7 @@
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="updateModalLabel">Update the main network</h5>
+                                        <h5 class="modal-title" id="updateModalLabel" style="color:black;">Update the main network</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
