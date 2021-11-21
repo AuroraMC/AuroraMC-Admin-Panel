6fbe83b2b0520d231564e2035867d8f4f3984d05
Block2Block <accounts@block2block.me>
2021-11-21 16:51:27 +0000

    It ain't working so i ain't breaking it again

--- mission-control/utils/server/restart.php
+++ mission-control/utils/server/restart.php
@@ -1,7 +1,4 @@
 <?php
-
-require '../socket-util.php';
-
 if (isset($_POST['server'], $_POST['network'])) {
     $server = filter_input(INPUT_POST, 'server', FILTER_SANITIZE_STRING);
     $network = filter_input(INPUT_POST, 'network', FILTER_SANITIZE_STRING);
@@ -10,5 +7,18 @@ if (isset($_POST['server'], $_POST['network'])) {
     $port = 35567;
     $data = "restartserver;". $network . ";" . $server ."\r\n";
 
-    socket_send($host, $port, $data);
+    if (($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === FALSE) {
+        echo "Failed to initialise socket.";
+    } else {
+        if (($result = socket_connect($socket, $host, $port)) === false) {
+            echo "Failed to create connection.";
+        } else {
+            socket_write($socket, $data, strlen($data));
+
+            while (($out = socket_read($socket, 2048)) != "") {
+                echo $out;
+            }
+        }
+        socket_close($socket);
+    }
 }
