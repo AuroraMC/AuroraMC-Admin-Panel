06a27e197690abb88944c602dc5fe29dad2a7522
Block2Block <accounts@block2block.me>
2021-11-21 14:35:27 +0000

    Removed white space

--- mission-control/js/main.js
+++ mission-control/js/main.js
@@ -1,12 +1,15 @@
 function networkChangeServerCreate(network) {
     if (network === 'MAIN') {
-        document.getElementById("extra_create_server").style.visibility = "visible";
-    } else {
         document.getElementById("extra_create_server").style.visibility = "hidden";
+        document.getElementById("extra_create_server").style.display = "none";
+    } else {
+        document.getElementById("extra_create_server").style.visibility = "visible";
+        document.getElementById("extra_create_server").style.display = "block";
     }
 }
 
 function serverCreate(server, network, game, extradetails) {
+
     $.ajax({
         url:'/mission-control/utils/create-server.php',
         type: 'post',
