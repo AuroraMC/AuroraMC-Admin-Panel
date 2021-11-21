835874d617a8f28b722394977c20a23545ec4fa4
Block2Block <accounts@block2block.me>
2021-11-21 18:33:44 +0000

    Fixed if-else

--- mission-control/js/main.js
+++ mission-control/js/main.js
@@ -1,20 +1,20 @@
 function networkChangeServerCreate(network) {
-    if (network === 'MAIN') {
-        document.getElementById("extra_create_server").style.visibility = "hidden";
-        document.getElementById("extra_create_server").style.display = "none";
-    } else {
+    if (network === 'TEST') {
         document.getElementById("extra_create_server").style.visibility = "visible";
         document.getElementById("extra_create_server").style.display = "block";
+    } else {
+        document.getElementById("extra_create_server").style.visibility = "hidden";
+        document.getElementById("extra_create_server").style.display = "none";
     }
 }
 
 function networkChangeProxyCreate(network) {
-    if (network === 'MAIN') {
-        document.getElementById("extra_create_proxy").style.visibility = "hidden";
-        document.getElementById("extra_create_proxy").style.display = "none";
-    } else {
+    if (network === 'TEST') {
         document.getElementById("extra_create_proxy").style.visibility = "visible";
         document.getElementById("extra_create_proxy").style.display = "block";
+    } else {
+        document.getElementById("extra_create_proxy").style.visibility = "hidden";
+        document.getElementById("extra_create_proxy").style.display = "none";
     }
 }
 
