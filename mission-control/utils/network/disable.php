ff714363dada0ee66cc016a964e40917e0e3dce2
BuildTools <unconfigured@null.spigotmc.org>
2021-11-24 12:11:45 +0000

    site done providing no issues

---
+++ mission-control/utils/network/disable.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_POST['network'])) {
+    $network = filter_input(INPUT_POST, 'network', FILTER_SANITIZE_STRING);
+
+    $host = "auroramc.block2block.me";
+    $port = 35567;
+    $data = "disablenetwork;". $network . "\r\n";
+
+    if (($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === FALSE) {
+        echo "Failed to initialise socket.";
+    } else {
+        if (($result = socket_connect($socket, $host, $port)) === false) {
+            echo "Failed to create connection.";
+        } else {
+            socket_write($socket, $data, strlen($data));
+
+            while (($out = socket_read($socket, 2048)) != "") {
+                echo $out;
+            }
+        }
+        socket_close($socket);
+    }
+}
