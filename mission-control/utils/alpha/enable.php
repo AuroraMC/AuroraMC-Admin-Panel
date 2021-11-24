024a0db8000adcc59bd2a2b07decd8539799158d
Block2Block <accounts@block2block.me>
2021-11-24 16:30:41 +0000

    Permissions stuff should now be done

--- mission-control/utils/alpha/enable.php
+++ mission-control/utils/alpha/enable.php
@@ -1,4 +1,15 @@
 <?php
+include_once '../../database/db-connect.php';
+
+$account_type = login_check($mysqli);
+if (!$account_type) {
+    header("Location: ../../login");
+}
+
+if ($account_type != "OWNER" && $account_type != "SR_DEV") {
+    header("Location: ../../login");
+}
+
     $host = "auroramc.block2block.me";
     $port = 35567;
     $data = "enablealpha\r\n";
