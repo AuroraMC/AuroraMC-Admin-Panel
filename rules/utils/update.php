0f1068c2fb9c731ea8e3e9a7fe6660539e820898
Block2Block <accounts@block2block.me>
2022-01-02 17:20:06 +0000

    There is now a button to update the rules

---
+++ rules/utils/update.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright (c) 2021 AuroraMC Ltd. All Rights Reserved.
+ */
+
+include_once '../../database/db-connect.php';
+include_once "../../utils/functions.php";
+
+sec_session_start();
+
+$account_type = login_check($mysqli);
+if (!$account_type) {
+    header("Location: ../../../login");
+}
+
+if ($account_type != "OWNER" && $account_type != "ADMIN" && $account_type != "SR_DEV" && $account_type != "RC") {
+    header("Location: ../../../login");
+}
+
+$host = "db.block2block.me";
+$port = 35567;
+$data = "updaterules\r\n";
+
+if (($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === FALSE) {
+    echo "Failed to initialise socket.";
+} else {
+    if (($result = socket_connect($socket, $host, $port)) === false) {
+        echo "Failed to create connection.";
+    } else {
+        socket_write($socket, $data, strlen($data));
+
+        while (($out = socket_read($socket, 2048)) != "") {
+            echo $out;
+        }
+    }
+    socket_close($socket);
+}
