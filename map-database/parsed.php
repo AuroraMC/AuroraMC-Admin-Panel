32b321d38cb1f56749717c6e7ca174218240f4ce
Block2Block <accounts@block2block.me>
2022-04-07 19:48:27 +0100

    Update logo link

--- map-database/parsed.php
+++ map-database/parsed.php
@@ -63,7 +63,7 @@ if ($account_type != "OWNER" && $account_type != "ADMIN" && $account_type != "SR
         </ul>
     </div>
     <div class="mx-auto my-2 order-0 order-md-1 position-relative">
-        <a class="mx-auto" href="/">
+        <a class="mx-auto" href="/map-database/">
             <img src="../img/logo.png" height="100px" width="100px"
                  style="margin-top:60px">
         </a>
