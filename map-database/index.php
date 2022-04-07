6674b77b2cf377143ae0b37dd09478c4ff9b8f6a
Block2Block <accounts@block2block.me>
2022-04-07 19:39:16 +0100

    Start map database

---
+++ map-database/index.php
@@ -0,0 +1,128 @@
+<?php
+/*
+ * Copyright (c) 2021 AuroraMC Ltd. All Rights Reserved.
+ */
+
+include_once "../database/db-connect.php";
+include_once "../utils/functions.php";
+
+sec_session_start();
+
+$account_type = login_check($mysqli);
+if (!$account_type) {
+    header("Location: ../../login");
+}
+
+if ($account_type != "OWNER" && $account_type != "ADMIN" && $account_type != "SR_DEV") {
+    header("Location: ../../login");
+}
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Home | Map Database | The AuroraMC Network</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript"
+            src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+
+    <link rel="stylesheet" href="css/navbar.css">
+
+    <link rel="icon"
+          type="image/png"
+          href="../img/logo.png">
+</head>
+
+<body style="background-color: #23272A;color:white">
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-1 order-md-0">
+        <ul class="navbar-nav ml-auto text-center">
+            <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="parsed">Parsed Maps</a>
+            </li>
+        </ul>
+    </div>
+    <div class="mx-auto my-2 order-0 order-md-1 position-relative">
+        <a class="mx-auto" href="/">
+            <img src="../img/logo.png" height="100px" width="100px"
+                 style="margin-top:60px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 dual-collapse2 order-2 order-md-2">
+        <ul class="navbar-nav mr-auto text-center">
+            <li class="nav-item">
+                <a class="nav-link" href="live">Live Maps</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="update">Push Update</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container-fluid" style="padding-top: 40px">
+    <div class="row">
+        <div class="col-sm-2"></div> <!-- Gap at left side of form -->
+        <div class="col-sm-8 col-xs-12">
+            <br>
+            <h1><Strong>AuroraMC Network Chat Filter Admin Panel</Strong></h1>
+            <br>
+            <legend style="font-family: 'Helvetica';">Welcome!</legend>
+            <hr>
+            <p style="font-size: 17px; font-family: 'Helvetica'">Welcome to the AuroraMC Network's Map Database! Here, you can see and manage live maps, view parsed but not live maps and push map updates to the network!.</p>
+            <br>
+            <legend style="font-family: 'Helvetica';">Map Statistics</legend>
+            <hr>
+            <?php
+            if ($sql = $mysqli->prepare("SELECT count(*) FROM maps WHERE parse_version = 'LIVE'")) {
+                $sql->execute();
+                $results2 = $sql->get_result();
+                $results = $results2->fetch_array(MYSQLI_NUM);
+                $results2->free_result();
+                $sql->free_result();
+                $maps = $results[0];
+                echo '<p><strong style="font-weight: bold">Total Live Maps:</strong> ', $maps,'</p>';
+            } else {
+                echo 'An error occurred when trying to connect to the database. Please try again.';
+            }
+            if ($sql = $mysqli->prepare("SELECT count(*) FROM maps WHERE parse_version = 'TEST'")) {
+                $sql->execute();
+                $results2 = $sql->get_result();
+                $results = $results2->fetch_array(MYSQLI_NUM);
+                $results2->free_result();
+                $sql->free_result();
+                $maps = $results[0];
+                echo '<p><strong style="font-weight: bold">Total Parsed Maps:</strong> ', $maps,'</p>';
+            } else {
+                echo 'An error occurred when trying to connect to the database. Please try again.';
+            }
+            ?>
+        </div>
+        <div class="col-sm-2"></div> <!-- Gap at right side of form -->
+    </div>
+</div>
+</body>
+</html>
